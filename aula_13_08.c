2253f0ffe5d66b83fc2cef6fcbb3a1dfce9861b9
Gabriele Zampier de Andrade <gabrielezampier36@gmail.com>
2025-08-14 16:35:21 -0300

    Update aula_13_08.c

---
+++ aula_13_08.c
@@ -0,0 +1,12 @@
+//Aula 13/08/2025 
+
+#include <stdio.h>
+int main(void){
+
+    printf("#######################\n");
+    printf("#######Helo Word#######\n");
+    printf("#######################\n");
+
+    return 0;
+}
+
